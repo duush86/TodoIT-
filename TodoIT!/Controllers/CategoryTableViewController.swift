0bbc34224cb532f00d550287a9b3885422d5882f
Antonio <j.antonio.oro@gmail.com>
2019-08-20 16:10:25 -0500

    Persisting data using Realm

--- TodoIT!/Controllers/CategoryTableViewController.swift
+++ TodoIT!/Controllers/CategoryTableViewController.swift
@@ -23,49 +23,35 @@ class CategoryTableViewController: UITableViewController {
     //MARK - TableView add new categories
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        // declares textField variable to host what the user's input
         var textField = UITextField()
-        // alert constant will host an alertController view element
         let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
-        // action handles the event when the user taps on add element inside the alert alement.
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            //what will happen once the user clicks the add item alert in our alert
-            //creates a new instance of the item clasdsadsa
+            
             let newCategory = Category()
-            //assigns .name the value hosted on textField.text
             newCategory.name = textField.text!
-            //appends the new category element on the itemArray array
-            //self.categoryArray.append(newCategory)
-            // calls saveData function
+           
             self.saveCategory(category: newCategory)
             
         }
-        // prints alert on screen
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new category"
             textField = alertTextField
         }
-        // prints and links the alert with the action method
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
   
     //MARK - TableView datasource methods
-    // numberOfRowsInSection method that prints all the cells for every item on itemArray
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories?.count ?? 1
     }
     
-    // cellForRowAt method that is used to set the text for every cell element
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // reference to the ToDoItemCell element
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        // pulls every item on itemArray
-        //let category = categoryArray[indexPath.row]
-        // assigns the item.title text to the cell element's text
+      
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories yet"
-        //print("The name is \(categories[indexPath.row].name)")
+    
         return cell
     }
     //MARK - TableView delegate methods
--- TodoIT!/Controllers/ToDoListViewController.swift
+++ TodoIT!/Controllers/ToDoListViewController.swift
@@ -11,7 +11,6 @@ import RealmSwift
 
 class ToDoListViewController: UITableViewController {
 
-    // setting up itemArray, that will be used to host the items retrieved from Items.plist and the new items.
     let realm = try! Realm()
     
     var todoItems: Results<Item>?
@@ -29,26 +28,21 @@ class ToDoListViewController: UITableViewController {
     }
     
     //MARK - Tableview Datasource Methods
-    // numberOfRowsInSection method that prints all the cells for every item on items
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoItems?.count ?? 1
     }
     
-    // cellForRowAt method that is used to set the text for every cell element
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // reference to the ToDoItemCell element
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
 
             if let item = todoItems?[indexPath.row] {
                 cell.textLabel?.text = item.title
                 cell.accessoryType = item.done ? .checkmark : .none
-               // print(item)
                 
             } else {
                 
                 cell.textLabel?.text = "No items under this category"
-                //print("No items here")
             }
      
         return cell
@@ -73,13 +67,9 @@ class ToDoListViewController: UITableViewController {
     }
     
     //MARK - Add new Items
-    //this method will handle the event when the user taps on the "+" button on the user interface
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        // declares textField variable to host what the user's input
         var textField = UITextField()
-        // alert constant will host an alertController view element
         let alert = UIAlertController(title: "Add new TodoIT! item", message: "", preferredStyle: .alert)
-        // action handles the event when the user taps on add element inside the alert alement.
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
     
             if let currentCategory = self.selectedCategorie {
@@ -89,6 +79,8 @@ class ToDoListViewController: UITableViewController {
                     let newItem = Item()
                     newItem.title = textField.text!
                     newItem.done = false
+                    newItem.createdDate = Date()
+                    
                     currentCategory.items.append(newItem)
                     
                     }} catch {
@@ -98,12 +90,10 @@ class ToDoListViewController: UITableViewController {
             self.tableView.reloadData()
         }
         
-       // prints alert on screen
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
         }
-        // prints and links the alert with the action method
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
@@ -120,20 +110,19 @@ class ToDoListViewController: UITableViewController {
 }
 
 //MARK: - Search bar methods
-//extension ToDoListViewController: UISearchBarDelegate {
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        let request : NSFetchRequest<Item> = Item.fetchRequest()
-//        let predicate = NSPredicate(format: "title CONTAINS %@", searchBar.text!)
-//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-//        loadItems(with: request, predicate: predicate)
-//    }
-//    
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        if searchBar.text?.count == 0 {
-//            loadItems()
-//            DispatchQueue.main.async {
-//                searchBar.resignFirstResponder()
-//            }
-//        }
-//    }
-//}
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "createdDate", ascending: true)
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
